f4ac61ff109d78a01eb0a6757c63b1ccee4c4964
M4rthin9 <pongsinbas@gmail.com>
2026-06-26 11:14:31 +0700

    update

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx_nDP6wByWIUgSpb8oJoESkF1ISKsZ5eQy4B5R76uehGGsyS-N5bkpWTAQ52s8hwpE8A/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzv7yvxTM9F_UM1Ch8bNP7EV0uD02U0h8afbzqwK4zhvTGqCTI6v6DVPBJDZ-qhv3HYOA/exec';
 const QUOTA = 20;
