209290d173692d8a20b9fabb6746a7b932e940dd
M4rthin9 <pongsinbas@gmail.com>
2026-07-09 06:19:09 +0700

    fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-let APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbze8DqoZiPksNs89j7Udn-2jpA6QXSAxftd4yj-a3JYNJ58kWW1PI27kLbtHrFIPHGymg/exec';
+let APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzRLiIiunJ5HxJE9MPd4nPInPtoqU2VjdEP76aJpkRjPQWDbbouaGHVtdrMo95O_wGOHA/exec';
 const QUOTA = 20;
 const BACKEND_DISCOVERED_KEY = 'gas_discovered_url';
 const RESOLVED_URL_KEY = 'cc_resolved_url';
